ac534dad19c4be5d5c65957c5ea2a8933c587799
Bayu Ramadhan <127529207+bayramdhan@users.noreply.github.com>
2023-03-21 21:41:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kopi Teras Senja</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;700&display=swap" rel="stylesheet">
+
+    <!-- Feather Icon -->
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <!-- My style -->
+
+</head>
+
+<body>
+    <!-- Navbar start -->
+    <nav class="navbar">
+        <a href="#" class="navbar-logo">Teras<span>senja</span>.</a>
+
+        <div class="navbar-nav">
+            <a href="#home">Home</a>
+            <a href="#about">Tentangkami</a>
+            <a href="#menu">Menu</a>
+            <a href="#contact">Kontak</a>
+        </div>
+
+        <div class="navbar-extra">
+            <a href="#" id="search"><i data-feather="search"></i></a>
+            <a href="#" id="shopping-cart"><i data-feather="shopping-cart"></i></a>
+            <a href="#" class="hamburger-menu" id="hamburger-menu"><i data-feather="menu"></i></a>
+        </div>
+    </nav>
+    <!-- Navbar end -->
+
+    <!-- Hero section start -->
+    <section class="hero" id="home">
+        <main class="content">
+            <h1>Mari Nikmati Secangkir <span>kopi</span></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <a href="#" class="cta">Beli Sekarang</a>
+        </main>
+    </section>
+    <!-- Hero section end -->
+
+    <!-- About Section start -->
+    <section id="about" class="about">
+        <h2><span>Tentang</span> Kami</h2>
+        <div class="row">
+            <div class="about-img">
+                <img src="img/Tentang-Kami.jpg" alt="Tentang Kami">
+            </div>
+            <div class="content">
+                <h3>Kenapa memilih kopi kami</h3>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus quidem quis iusto dolor at, perspiciatis deleniti cum adipisci dicta laudantium molestias possimus consequatur labore, maiores numquam esse, beatae vero explicabo.</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, quos libero autem alias vel doloremque perferendis repellendus vero facere mollitia aliquid provident nulla necessitatibus deserunt, iusto eum omnis! Libero, ad.</p>
+            </div>
+        </div>
+    </section>
+    <!-- About Section end -->
+
+    <!-- Menu Section start -->
+    <section id="menu" class="menu">
+        <h2><span>Menu</span> Kami</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur obcaecati officia illo et reprehenderit porro.</p>
+
+        <div class="row">
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+            <div class="menu-card">
+                <img src="img/menu/Latte art.jpg" alt="Latte art" class="menu-card-img">
+                <h3 class="menu-card-title">- Latte Art -</h3>
+                <p class="menu-card-price">IDR 15k</p>
+            </div>
+        </div>
+    </section>
+    <!-- Menu Section end-->
+    <!-- Contact Section start -->
+    <section id="contact" class="contact">
+        <h2><span>Kontak</span> Kami</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+        <div class="row">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.9225589599012!2d109.58991132918891!3d-7.045640799681933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e701fc9b1a5c951%3A0x4c8657548763e682!2sKajen%2C%20Prendengan%2C%20Sinangoh%20Prendeng%2C%20Kec.%20Kajen%2C%20Kabupaten%20Pekalongan%2C%20Jawa%20Tengah%2051161!5e0!3m2!1sid!2sid!4v1679371016883!5m2!1sid!2sid" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+
+            <form action="">
+                <div class="input-group">
+                    <i data-feather="user"></i>
+                    <input type="text" placeholder="nama">
+                </div>
+                <div class="input-group">
+                    <i data-feather="mail"></i>
+                    <input type="text" placeholder="email">
+                </div>
+                <div class="input-group">
+                    <i data-feather="phone"></i>
+                    <input type="text" placeholder="no hp">
+                </div>
+                <button type="submit" class="btn">kirim pesan</button>
+            </form>
+
+        </div>
+
+    </section>
+    <!-- Contact Section end -->
+
+    <!-- Footer strat -->
+    <footer>
+        <div class="socials">
+            <a href="#"><i data-feather="instagram"></i></a>
+            <a href="#"><i data-feather="twitter"></i></a>
+            <a href="#"><i data-feather="facebook"></i></a>
+        </div>
+
+        <div class="links">
+            <a href="#home">home</a>
+            <a href="#about">Tentang Kami</a>
+            <a href="#menu">Menu</a>
+            <a href="#contact">Kontak</a>
+        </div>
+
+        <div class="credit">
+            <p>Created by <a href="">bayuramadhan</a>. | &copy; 2023.</p>
+        </div>
+    </footer>
+    <!-- Footer end -->
+    <!-- Feather Icon -->
+    <script>
+        feather.replace()
+    </script>
+
+    <!-- My Javascrip -->
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
